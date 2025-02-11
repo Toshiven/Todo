797dd429706192e05b1243f0361dd86d8a2b2521
Toshiven <kevin.abler19@gmail.com>
2025-02-11 11:15:46 +0800

    style: made basic layout for login

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -7,23 +7,39 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: const Color(0xFF1e1e2e),
-        foregroundColor: const Color(0xFFcdd6f4),
-        title: Text("LOGIN"),
-      ),
       body: Center(
-        child: ElevatedButton(
-          child: Text("Login"),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const TodoApp()),
-            );
-          },
-        ),
-      ),
+          child: Card(
+              margin: EdgeInsets.all(20.0),
+              child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        child: Text("Login"),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const TodoApp()),
+                          );
+                        },
+                      ),
+                    ],
+                  )))),
       backgroundColor: const Color(0xFF1e1e2e),
     );
   }
